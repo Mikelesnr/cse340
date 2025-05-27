ad5fa9066bc910df79c775a8f181d1aa8a5ae66e
Michael Mwanza <mwanza.n.m@gmail.com>
2025-05-27 11:32:59 +0200

    index error fixed

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,6 +3,6 @@ const baseController = {};
 
 baseController.buildHome = async function (req, res) {
   const nav = await utilities.getNav();
-  res.render("indexx", { title: "Home", nav });
+  res.render("index", { title: "Home", nav });
 };
 module.exports = baseController;
