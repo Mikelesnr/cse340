d4e83b1b4356b95d8ca4f73457928e758cf5d80f
Michael Mwanza <mwanza.n.m@gmail.com>
2025-05-27 11:31:34 +0200

    robust error handling added

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,5 +1,5 @@
 const invModel = require("../models/inventory-model");
-const utilities = require("../utilities/");
+const utilities = require("../utilities/index");
 
 const invCont = {};
 
@@ -7,16 +7,37 @@ const invCont = {};
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId;
-  const data = await invModel.getInventoryByClassificationId(classification_id);
-  const grid = await utilities.buildClassificationGrid(data);
-  let nav = await utilities.getNav();
-  const className = data[0].classification_name;
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  });
+  try {
+    const classification_id = req.params.classificationId;
+    const data = await invModel.getInventoryByClassificationId(
+      classification_id
+    );
+    console.log("Data from invModel.getInventoryByClassificationId:", data);
+
+    // Handle case where data is empty or undefined
+    if (!data || data.length === 0) {
+      return next({
+        status: 404,
+        message: "No vehicles found for the given classification.",
+      });
+    }
+
+    const grid = await utilities.buildClassificationGrid(data);
+    let nav = await utilities.getNav();
+    const className = data[0].classification_name;
+
+    res.render("./inventory/classification", {
+      title: className + " vehicles",
+      nav,
+      grid,
+    });
+  } catch (error) {
+    console.error("Error in buildByClassificationId:", error);
+    next({
+      status: error.status || 500,
+      message: "Something went wrong while fetching classification data.",
+    });
+  }
 };
 
 module.exports = invCont;
